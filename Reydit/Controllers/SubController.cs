c4fc398e9a3556b96570c04de4208a4756bcdf4c
mrrey <mrrey@REY-PC>
2016-11-25 17:41:15 +0100

    Added Subreydit Creation

--- Reydit/Controllers/SubController.cs
+++ Reydit/Controllers/SubController.cs
@@ -45,5 +45,15 @@ namespace Reydit.Controllers
             return Content("There was an error with the request.");
         }
 
+        [Route("r/{shortname}")]
+        public ActionResult Show(string shortname)
+        {
+            var rc = ReyditContext.Current;
+            var sub = rc.Subreydits.Where(o => o.ShortName.ToLower() == shortname.ToLower()).First();
+            if (sub == null)
+                return Content("<h2>Error!</h2>A subreydit with that name doesn't exist!", "text/html");
+            return View(sub);
+        }
+
     }
 }
